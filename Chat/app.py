ed96e33e5944520125808450432ad2615cfff5a8
Ignacio Brasca <warkanlock@gmail.com>
2020-03-27 19:39:26 -0300

    Update app.py

--- Chat/app.py
+++ Chat/app.py
@@ -5,6 +5,10 @@ from flask_socketio import join_room, leave_room
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+room = {
+    "tomas" : "room1"
+    "username" : "room2"
+}
 
 @app.route('/')
 def index():
@@ -14,20 +18,24 @@ def index():
 @socketio.on('submit msg')
 def msg(data):
     selection = data["selection"]
-    emit("show msg", {"selection": selection}, broadcast=True)
+    room = data["room"]
+    user = data["user"]
+    emit("show msg", {"selection": selection, "room" : room[user]}, broadcast=True)
 
 
-# @socketio.on('join')
-# def on_join(data):
-#    username = data['username']
-#    room = data['room']
-#    join_room(room)
-#    send(username + ' has entered the room.', room=room)
+@socketio.on('join')
+def on_join(data):
+   username = data['username']
+   room = data['room']
+   room["username"] = room;
+   join_room(room)
+   send(username + ' has entered the room.', room=room)
 
 
-# @socketio.on('leave')
-# def on_leave(data):
-#    username = data['username']
-#    room = data['room']
-#    leave_room(room)
-#    send(username + ' has left the room.', room=room)
+@socketio.on('leave')
+def on_leave(data):
+   username = data['username']
+   room = data['room']
+   room.pop(username)
+   leave_room(room)
+   send(username + ' has left the room.', room=room)
